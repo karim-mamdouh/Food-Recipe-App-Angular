e557580fec1daf64d92efce54e6211fd57d7b9ab
Yasmeen-Mostafa <yasmeen.mostafa1236@gmail.com>
2022-07-06 19:05:50 +0200

    Added localstorage authentication for login and register

--- src/app/modules/auth-module/register/register.component.ts
+++ src/app/modules/auth-module/register/register.component.ts
@@ -1,26 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ConfirmPasswordValidator } from './confirm-password.valdators';
+import { ConfirmPasswordValidator } from './validators/confirm-password.valdators';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
+  showErrors: boolean = false;
   gender = [
     { name: 'Male', value: 'm' },
     { name: 'Female', value: 'f' },
-  ];
-  selectedGender: any;
-  value: Date = new Date();
+  ]; //List of genders
   registerForm: FormGroup = this._registerFormBuilder.group(
     {
       firstName: [
         '',
         [Validators.required, Validators.pattern(/^[\S][A-Za-z]{2,}$/)],
       ],
-      date: [new Date(), [Validators.required]],
-      gender: [{}, [Validators.required]],
+      date: ['', [Validators.required]],
+      gender: ['', [Validators.required]],
       lastName: [
         '',
         [Validators.required, Validators.pattern(/^[\S][A-Za-z]{2,}$/)],
@@ -28,7 +27,7 @@ export class RegisterComponent implements OnInit {
       userName: [
         '',
         [Validators.required, Validators.pattern(/^[\S][A-Za-z0-9]{5,}$/)],
-      ], //no spaces,2 char or more,no special character
+      ], //no spaces,6 char or more,no special character
       email: [
         '',
         [
@@ -44,17 +43,15 @@ export class RegisterComponent implements OnInit {
           Validators.required,
           Validators.pattern(
             /^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})./
-          ),
+          ), //At least one lowercase, at least one uppercase or one number, minimum 6 characters
         ],
       ],
-      checkBox: [false, []],
       confirmPassword: ['', [Validators.required]],
     },
     {
       validators: ConfirmPasswordValidator('password', 'confirmPassword'),
     }
   ); //Form group having all inputs with validators
-  //create array of objects contain genders
 
   get controlValidation() {
     return this.registerForm.controls;
@@ -62,7 +59,28 @@ export class RegisterComponent implements OnInit {
   constructor(private _registerFormBuilder: FormBuilder) {}
 
   ngOnInit(): void {}
-  submitReactiveForm(): any {
-    return this.registerForm.controls;
+  submitReactiveForm(): void {
+    //Check for register form if not valid
+    if (this.registerForm.status !== 'INVALID') {
+      //Get from local storage the users array if found
+      let getUsers = localStorage.getItem('users');
+      if (getUsers) {
+        //If users found
+        let temp = JSON.parse(getUsers as string); //Convert getUsers to object
+        temp.push(this.registerForm.value); //Add new user to users array in local storage
+        localStorage.setItem('users', JSON.stringify(temp)); //Update users array in local storage
+      } else {
+        //if users not found in local storage then create one and add new user to it
+        localStorage.setItem(
+          'users',
+          JSON.stringify([this.registerForm.value])
+        );
+      }
+      //Empty form data
+      this.registerForm.reset();
+      this.showErrors = false;
+    } else {
+      this.showErrors = true;
+    }
   }
 }
