759609375f8c490c99bd74f4a0d30fe6cd4027bb
MahmoudBadawy4199 <m.badawy2011@gmail.com>
2022-07-08 19:04:46 +0200

    reciptDetails (Api + routing)

--- src/app/modules/food-module/food-module-routing.module.ts
+++ src/app/modules/food-module/food-module-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FoodrecipeComponent } from './foodrecipe/foodrecipe.component';
 import { HomepageComponent } from './homepage/homepage.component';
 
 const routes: Routes = [
   {path:'homepage',component:HomepageComponent},
+  {path:'recipe/:id',component:FoodrecipeComponent},
 ];
 
 @NgModule({
--- src/app/modules/food-module/foodrecipe/foodrecipe.component.ts
+++ src/app/modules/food-module/foodrecipe/foodrecipe.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { RecipeDetails } from 'src/app/interfaces/food-category';
+import { FoodAPIService } from 'src/app/services/food-api.service';
 
 @Component({
   selector: 'app-foodrecipe',
   templateUrl: './foodrecipe.component.html',
   styleUrls: ['./foodrecipe.component.scss']
 })
-export class FoodrecipeComponent implements OnInit {
 
-  constructor() { }
+export class FoodrecipeComponent implements OnInit {
+  recipeID!: string;
+  recipeDetails = {} as RecipeDetails;
+
+  constructor(
+    private _router: ActivatedRoute,
+    private _foodApiService: FoodAPIService,
+  ) { }
 
   ngOnInit(): void {
+    // Recieve the recipt ID
+    this._router.queryParams.subscribe((params) => {
+      this.recipeID = params['recipeID'];
+      this.fetchDataFromApi(this.recipeID);
+    });
   }
 
+  fetchDataFromApi(recipeID: string) {
+    // Fetch food Recipt from api 
+    this._foodApiService.getSpecificRecipe(recipeID).subscribe(response => {
+      this.recipeDetails = response.recipe;
+    });
+  }
+  
 }
