78ada18f0e8e0f28f169a4bdc6688e16633c281a
Karim Mamdouh Mohamed <ghost_594@hotmail.com>
2022-07-10 12:51:27 +0200

    Added reset recipes action to store and reducer

--- src/app/store/food-recipies/food-recipies.reducers.ts
+++ src/app/store/food-recipies/food-recipies.reducers.ts
@@ -1,7 +1,7 @@
 import { createReducer, on } from '@ngrx/store';
 import { FoodCategories, Recipe } from 'src/app/interfaces/food-category';
 import { FoodReducerTemplate } from 'src/app/interfaces/store';
-import { fillRecipies } from './food-recipies.actions';
+import { fillRecipies, resetRecipes } from './food-recipies.actions';
 
 //Recipes initial state
 const initialState: FoodReducerTemplate = {
@@ -18,6 +18,13 @@ export const recipiesReducer = createReducer(
       ...state,
       recipes: [...state.recipes, ...action.payload.recipes],
       category: action.payload.category,
+    };
+  }),
+  //Reset recipes action
+  on(resetRecipes, (state) => {
+    return {
+      ...state,
+      recipes: [],
     };
   })
 );
