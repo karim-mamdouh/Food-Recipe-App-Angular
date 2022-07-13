019ce5b1a32b37b26a3abd7a85af9b8faed408db
Karim Mamdouh Mohamed <ghost_594@hotmail.com>
2022-07-13 23:10:07 +0200

    fixed wrong path for service worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -54,7 +54,7 @@ import { FilterRecieptsPipe } from './pipes/filter-reciepts.pipe';
       recipies: recipiesReducer,
       favourites: favouritesReducer,
     }),
-    ServiceWorkerModule.register('/ngsw-worker.js', {
+    ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
